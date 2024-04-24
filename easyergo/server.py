acfb12934e2eeba188bf235c4e55e75c60b97377
Mikael Öhman <micketeer@gmail.com>
2024-04-24 15:56:51 +0000

    Reorganize initalization of values read from easyconfig

--- easyergo/server.py
+++ easyergo/server.py
@@ -28,6 +28,15 @@ subtoolchains = {tc_class.NAME: getattr(tc_class, 'SUBTOOLCHAIN', None) for tc_c
 
 robot_paths = ['/apps/easybuild-easyconfigs/easybuild/easyconfigs/']
 
+
+def get_close_matches_icase(word, possibilities, *args, **kwargs):
+    """ Case-insensitive version of difflib.get_close_matches """
+    lword = word.lower()
+    lpos = {p.lower(): p for p in possibilities}
+    lmatches = difflib.get_close_matches(lword, lpos.keys(), *args, **kwargs)
+    return [lpos[m] for m in lmatches]
+
+
 def get_identifiers(tree):
     cursor = tree.walk()
     # TODO: skip going into nodes that are of type "attribute"
@@ -43,6 +52,7 @@ def get_identifiers(tree):
             visited_children = False
         elif not cursor.goto_parent():
             break
+
 
 def get_dependencies(tree):
     # Not very robust, assumes the simple case which is the case in 99% of easyconfigs
@@ -61,6 +71,7 @@ def get_dependencies(tree):
 
 def find_deps(name, versionsuffix, tcs):
     name_exists = True
+    name_suggestions = []
     matches = []
     for robot_path in robot_paths:
         for tc in tcs:
@@ -69,14 +80,15 @@ def find_deps(name, versionsuffix, tcs):
                 matches += glob(f'{robot_path}/{name}-*{versionsuffix}.eb')
             else:
                 tcname = tc['name'] + '-' + tc['version']
-                print(f'{robot_path}/{name[0].lower()}/{name}/{name}-*-{tcname}{versionsuffix}.eb')
                 matches += glob(f'{robot_path}/{name[0].lower()}/{name}/{name}-*-{tcname}{versionsuffix}.eb')
                 matches += glob(f'{robot_path}/{name}-*-{tcname}{versionsuffix}.eb')
     if not matches:  # Check if name exists at all
         name_exists = bool(glob(f'{robot_path}/{name[0].lower()}/{name}/')) or bool(glob(f'{robot_path}/{name}-*.eb'))
+        if not name_exists:
+            name_suggestions = [m.split('/')[-2] for m in glob(f'{robot_path}/{name[0].lower()}/*/')]
 
     matches = [match.split('/')[-1] for match in matches]
-    return matches, name_exists
+    return matches, name_exists, name_suggestions
 
 
 def make_diagnostic(node, message):
@@ -90,40 +102,53 @@ def make_diagnostic(node, message):
         source="EasyErgo")
 
 
+def extract(tree, names):
+    # TODO search for names as assigned identifiers
+    return {}
+
+
 server = LanguageServer("easyergo-server", "dev")
 
 @server.feature(types.TEXT_DOCUMENT_DID_SAVE)
 @server.feature(types.TEXT_DOCUMENT_DID_OPEN)
 async def check_known_kws(ls,  params=types.DocumentDiagnosticParams):
     """Checks keywords agains know eb keywords"""
+    
+    # Extract as much information as possible:
     text_doc = ls.workspace.get_text_document(params.text_document.uri)
-
-    # ec = EasyConfigParser(rawcontent=text_doc.source)
-    # ecdict = ec.get_config_dict()
-    # name = ec['ec']['name']
-    easyblock_name, name = fetch_parameters_from_easyconfig(text_doc.source, ['easyblock', 'name'])
-    logging.info(f'easyblock: {easyblock_name}, name: {name}')
+    tree = parser.parse(bytes(text_doc.source, 'utf8'))
     try:
-        app_class = get_easyblock_class(easyblock_name, name=name)
+        ec = EasyConfigParser(rawcontent=text_doc.source)
+        ecdict = ec.get_config_dict(validate=False)
+    except:
+        ecdict = extract(tree, ['easyblock', 'name', 'version', 'toolchain'])
+
+    logging.info(f'found: {ecdict}')
+       
+    try:
+        easyblock, name = ecdict['easyblock'], ecdict['name']
+        app_class = get_easyblock_class(easyblock, name=name)
         eb_kw = app_class.extra_options()
     except:
         eb_kw = []
-    all_known_ids = set(eb_kw) | set(default_parameters) | set(all_constants)
 
     # Detect toolchain
-    toolchain = fetch_parameters_from_easyconfig(text_doc.source, ['toolchain'])[0]
-    # TODO:
-    default_tcs = [{'name': 'foss', 'version': '2023a'},
-                   {'name': 'gfbf', 'version': '2023a'},
-                   {'name': 'gompi', 'version': '2023a'},
-                   {'name': 'GCC', 'version': '12.3.0'},
-                   {'name': 'GCCcore', 'version': '12.3.0'}]
+    if 'toolchain' in ecdict:
+        # TODO:
+        default_tcs = [{'name': 'foss', 'version': '2023a'},
+                       {'name': 'gfbf', 'version': '2023a'},
+                       {'name': 'gompi', 'version': '2023a'},
+                       {'name': 'GCC', 'version': '12.3.0'},
+                       {'name': 'GCCcore', 'version': '12.3.0'}]
+    else:
+        default_tcs = None
+
+    all_known_ids = set(eb_kw) | set(default_parameters) | set(all_constants)
 
     diagnostics = []
 
     # Check options and constants
     nodes = []
-    tree = parser.parse(bytes(text_doc.source, 'utf8'))
     for node in get_identifiers(tree):
         ident = node.text.decode('utf8')
         if ident.startswith('local_') or ident.startswith('_') or ident in builtin_functions :
@@ -133,7 +158,7 @@ async def check_known_kws(ls,  params=types.DocumentDiagnosticParams):
     for node in nodes:
         kw = node.text.decode('utf8')
         if kw not in default_parameters and kw not in eb_kw and kw not in all_constants:
-            matches = difflib.get_close_matches(kw, all_known_ids)
+            matches = get_close_matches_icase(kw, all_known_ids)
             message = "Did you mean: " + ",".join(matches) if matches else "Unknown variable"
             diagnostics.append(make_diagnostic(node, message))
 
@@ -150,7 +175,6 @@ async def check_known_kws(ls,  params=types.DocumentDiagnosticParams):
                 if values[0].type != 'string' or values[1].type != 'string':
                     continue  # ignoring parameterized entries
 
-                print(values[0].text, values[1].type)
                 name, version = eval(values[0].text), eval(values[1].text)
                 versionsuffix, tcs = '', default_tcs
                 if len(values) >= 3:
@@ -165,7 +189,7 @@ async def check_known_kws(ls,  params=types.DocumentDiagnosticParams):
                     else:
                         continue  # tcs = [eval(values[3].text)]  # Just giving up for now
 
-                matches, name_exists = find_deps(name, versionsuffix, tcs)
+                matches, name_exists, name_suggestions = find_deps(name, versionsuffix, tcs)
                 if matches:
                     if not any(version in match for match in matches):
                         diagnostics.append(make_diagnostic(values[1], 'Try ' + ','.join(matches)))
@@ -173,7 +197,19 @@ async def check_known_kws(ls,  params=types.DocumentDiagnosticParams):
                     if name_exists:
                         diagnostics.append(make_diagnostic(values[1], 'No compatible version exist'))
                     else:
-                        diagnostics.append(make_diagnostic(values[0], 'Name not recognized'))
+                        matches = get_close_matches_icase(name, name_suggestions)
+                        message = "Did you mean " + ",".join(matches) if matches else "Name not recognized"
+                        diagnostics.append(make_diagnostic(values[0], message))
+
+    # Check filename matching name, version, toolchain, versionsuffix
+    filename = params.text_document.uri.split('/')[-1]
+    for node in nodes:
+        if node.text == b'name':
+            pass #diagnostics.append(make_diagnostic(node, "Does not match filename"))
+        elif node.text == b'version':
+            pass #diagnostics.append(make_diagnostic(node, "Does not match filename"))
+        elif node.text == b'toolchain':
+            pass #diagnostics.append(make_diagnostic(node, "Does not match filename"))
 
     ls.publish_diagnostics(text_doc.uri, diagnostics)
 
